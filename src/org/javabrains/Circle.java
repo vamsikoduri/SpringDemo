09d0f34132ae9a322a6c4ea6d6795a34bb134758
vamc <vamc.koduri@gmail.com>
2016-10-24 08:44:42 +0530

    JSR 250 annotations

--- src/org/javabrains/Circle.java
+++ src/org/javabrains/Circle.java
@@ -1,5 +1,9 @@
 package org.javabrains;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
@@ -12,12 +16,29 @@ public class Circle implements Shape {
 		return center;
 	}
 
-	@Autowired
-	@Qualifier("circleQualifier")
+	/*@Autowired
+	@Required
+	@Qualifier("circleQualifier")*/
+	@Resource(name="pointC")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
 
+	
+	@PostConstruct
+	public void afterInit()
+	{
+	System.out.println("*****After Initialization of Circle object ****");	
+	}
+	
+	@PreDestroy
+	public void beforeDestroy()
+	{
+		System.out.println("*****Before destruction of Circle object ****");	
+			
+	}
+	
+	
 	@Override
 	public void draw() {
 		System.out.println("Drawing a circle !!");
