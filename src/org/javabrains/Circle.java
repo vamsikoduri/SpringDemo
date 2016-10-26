c9211790d50d9765e3f38549f185dbb3cccb234d
vamc <vamc.koduri@gmail.com>
2016-10-26 09:24:23 +0530

    Event handling in spring

--- src/org/javabrains/Circle.java
+++ src/org/javabrains/Circle.java
@@ -7,18 +7,22 @@ import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 @Component
-public class Circle implements Shape {
+public class Circle implements Shape, ApplicationEventPublisherAware {
 
 	Point center;
-	
+	private ApplicationEventPublisher publisher;
+
 	@Autowired
 	private MessageSource messageBean;
-	
+
 	public MessageSource getMessageBean() {
 		return messageBean;
 	}
@@ -31,38 +35,46 @@ public class Circle implements Shape {
 		return center;
 	}
 
-	/*@Autowired
-	@Required
-	@Qualifier("circleQualifier")*/
-	@Resource(name="pointC")
+	/*
+	 * @Autowired
+	 * 
+	 * @Required
+	 * 
+	 * @Qualifier("circleQualifier")
+	 */
+	@Resource(name = "pointC")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
 
-	
 	@PostConstruct
-	public void afterInit()
-	{
-	System.out.println("*****After Initialization of Circle object ****");	
-	}
-	
-	@PreDestroy
-	public void beforeDestroy()
-	{
-		System.out.println("*****Before destruction of Circle object ****");	
-			
-	}
-	
-	
-	@Override
-	public void draw() {
-		System.out.println(this.messageBean.getMessage("circle.draw", null,"DefaultGreetings From circel draw",null));
-		System.out.println(this.messageBean.getMessage("point", new Object[]{center.getX(),center.getY()},"DefaultGreetings From Point",null));
-		//System.out.println("Drawing a circle !!");
-		//System.out.println("Point: ( " + center.getX() + ", " + center.getY() + " )");
-		System.out.println(this.messageBean.getMessage("greetings", null,"DefaultGreetings",null));
+	public void afterInit() {
+		System.out.println("*****After Initialization of Circle object ****");
 	}
 
-	
-	
+	@PreDestroy
+	public void beforeDestroy() {
+		System.out.println("*****Before destruction of Circle object ****");
+
+	}
+
+	@Override
+	public void draw() {
+		DrawEvent d = new DrawEvent(this);
+		publisher.publishEvent(d);
+		System.out.println(this.messageBean.getMessage("circle.draw", null, "DefaultGreetings From circel draw", null));
+		System.out.println(this.messageBean.getMessage("point", new Object[] { center.getX(), center.getY() },
+				"DefaultGreetings From Point", null));
+		// System.out.println("Drawing a circle !!");
+		// System.out.println("Point: ( " + center.getX() + ", " + center.getY()
+		// + " )");
+		System.out.println(this.messageBean.getMessage("greetings", null, "DefaultGreetings", null));
+			}
+
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		this.publisher = publisher;
+
+	}
+
 }
